b14ae45a8ca58f19c546150bd2ecfd22e02e6d41
soncomqiq <idgamescs@hotmail.com>
2022-11-04 00:13:44 +0700

    lab-r1-l7

--- src/components/StudentItem.js
+++ src/components/StudentItem.js
@@ -1,11 +1,15 @@
 import "./StudentItem.css";
 
 function StudentItem() {
+  const name = "Nuttachai";
+  const surname = "Kulthammanit";
+  const age = 26;
+
   return (
       <div className="StudentItem">
-        <div>Nuttachai</div>
-        <div>Kulthammanit</div>
-        <div>26</div>
+        <div>{name}</div>
+        <div>{surname}</div>
+        <div>{age}</div>
       </div>
   )
 }
