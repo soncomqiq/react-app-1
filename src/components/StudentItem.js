c0664c580c0ca7778350551cbe99b9fcfc01daba
soncomqiq <idgamescs@hotmail.com>
2022-11-03 23:24:14 +0700

    lab-r1-l5

--- src/components/StudentItem.js
+++ src/components/StudentItem.js
@@ -1,5 +1,11 @@
 function StudentItem() {
-  return <h2>Student Item</h2>
+  return (
+      <div>
+        <div>Nuttachai</div>
+        <div>Kulthammanit</div>
+        <div>26</div>
+      </div>
+  )
 }
 
 export default StudentItem;
