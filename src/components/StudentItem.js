dfc3a1a9007f17a9a5cb05592018832cfa45e788
soncomqiq <idgamescs@hotmail.com>
2022-11-06 01:27:12 +0700

    lab-r1-l9

--- src/components/StudentItem.js
+++ src/components/StudentItem.js
@@ -1,12 +1,14 @@
 import "./StudentItem.css";
 
 function StudentItem(props) {
+  let retired = props.age >= 60;
 
   return (
       <div className="StudentItem">
         <div>{props.name}</div>
         <div>{props.surname}</div>
         <div>{props.age}</div>
+        <div className={retired ? "Tag Retired" : "Tag Working"}>{retired ? "Retired" : "Working"}</div>
       </div>
   )
 }
