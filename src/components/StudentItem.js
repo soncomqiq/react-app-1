28bc7d6e8dbd839435aa0fceb975cb15deb71945
soncomqiq <idgamescs@hotmail.com>
2022-11-08 00:30:31 +0700

    lab-r1-l10

--- src/components/StudentItem.js
+++ src/components/StudentItem.js
@@ -1,14 +1,36 @@
 import "./StudentItem.css";
 
 function StudentItem(props) {
-  let retired = props.age >= 60;
+  let age = props.age;
+  let retired = age >= 60;
+  let year = new Date().getFullYear() - age;
+
+  let yearClassName = "Tag ";
+  const reminder = year % 4;
+
+  switch (reminder) {
+    case 1:
+      yearClassName += "Red"
+      break;
+    case 2:
+      yearClassName += "Green"
+      break;
+    case 3:
+      yearClassName += "LightBlue"
+      break;
+    default:
+      yearClassName += "Blue"
+  }
 
   return (
       <div className="StudentItem">
         <div>{props.name}</div>
         <div>{props.surname}</div>
-        <div>{props.age}</div>
-        <div className={retired ? "Tag Retired" : "Tag Working"}>{retired ? "Retired" : "Working"}</div>
+        <div>{age}</div>
+        <div className="AgeGroup">
+          <div className={retired ? "Tag Retired" : "Tag Working"}>{retired ? "Retired" : "Working"}</div>
+          <div className={yearClassName}>{year}</div>
+        </div>
       </div>
   )
 }
