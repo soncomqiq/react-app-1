e389d075f96678a51d5a75ba55d6aa7c0cddb843
soncomqiq <idgamescs@hotmail.com>
2023-02-26 01:54:48 +0700

    lab-r2-l3

--- src/components/StudentItem.js
+++ src/components/StudentItem.js
@@ -1,15 +1,22 @@
 import "./StudentItem.css";
 import StudentTags from "./StudentTags";
+import {useState} from "react";
 
 function StudentItem(props) {
   let age = props.age;
+  const [name, setName] = useState(props.name);
+
+  const nameHandler = () => {
+    setName("Changed");
+  }
 
   return (
       <div className="StudentItem">
-        <div>{props.name}</div>
+        <div>{name}</div>
         <div>{props.surname}</div>
         <div>{age}</div>
         <StudentTags age={age}/>
+        <button onClick={nameHandler}>Click</button>
       </div>
   )
 }
