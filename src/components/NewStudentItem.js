8e4739e99d4b1c709d6e43892d9fe0cb7c28c075
soncomqiq <idgamescs@hotmail.com>
2023-02-26 14:37:13 +0700

    lab-r2-l5

--- src/components/NewStudentItem.js
+++ src/components/NewStudentItem.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
 const NewStudentItem = () => {
+  const [currentName, setCurrentName] = useState('');
+  const [currentLastName, setCurrentLastName] = useState('');
+  const [currentAge, setCurrentAge] = useState('');
+  const nameChangeHandler = (event) => {
+    setCurrentName(event.target.value)
+  }
+
+  const lastNameChangeHandler = (event) => {
+    setCurrentLastName(event.target.value)
+  }
+
+  const ageChangeHandler = (event) => {
+    setCurrentAge(event.target.value)
+  }
 
   return (
       <form>
         <div>
           <div className="StudentInput">
             <label>Name</label>
-            <input type="text"/>
+            <input type="text" onChange={nameChangeHandler}/>
           </div>
           <div className="StudentInput">
             <label>Lastname</label>
-            <input type="text"/>
+            <input type="text" onChange={lastNameChangeHandler}/>
           </div>
           <div className="StudentInput">
             <label>Age</label>
-            <input type="number" min="1" max="99" step="1"/>
+            <input type="number" min="1" max="99" step="1" onChange={ageChangeHandler}/>
           </div>
         </div>
         <div className="SubmitButton">
